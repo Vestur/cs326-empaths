fa616e1ae18bda751b45124ab83ba38d72478bf5
Parsua <patoofi@umass.edu>
2022-04-18 12:57:14 -0400

    updated sidebar html for all pages

---
+++ static/donations.js
@@ -0,0 +1,20 @@
+
+//side-bar buttons & event listeners --> to switch between pages
+//window.location.href = "search.html";
+const settingsB = document.getElementById("settings");
+settingsB.addEventListener('click', );
+
+const listsB = document.getElementById("my_lists");
+listsB.addEventListener('click', );
+
+const favoritesB = document.getElementById("my_favorites");
+favoritesB.addEventListener('click', );
+
+const likesB = document.getElementById("my_likes");
+likesB.addEventListener('click', );
+
+const donationsB = document.getElementById("my_donations");
+donationsB.addEventListener('click', );
+
+const signoutB = document.getElementById("signout");
+signoutB.addEventListener('click', );
