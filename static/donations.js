b93a0fe0ad744da0b4da80954e70329aa44f1b01
Parsua <patoofi@umass.edu>
2022-04-18 14:14:35 -0400

    fixing donations table

--- static/donations.js
+++ static/donations.js
@@ -1,2 +1,10 @@
 const addrowB = document.getElementById("add_row");
-addrowB.addEventListener('click', );
+addrowB.addEventListener('click', () => {
+    const table = document.getElementById("my_table");
+    table.insertRow(0);
+    let name = row.insertCell(0);
+    let amount = row.insertCell(1);
+    let date = row.insertCell(2);
+
+
+});
