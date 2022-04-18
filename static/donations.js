68449066d7ff91b1411cba6b24611674335d75bc
Parsua <patoofi@umass.edu>
2022-04-18 13:40:35 -0400

    sidebar allows page navigation now

--- static/donations.js
+++ static/donations.js
@@ -1,20 +1,2 @@
-
-//side-bar buttons & event listeners --> to switch between pages
-//window.location.href = "search.html";
-const settingsB = document.getElementById("settings");
-settingsB.addEventListener('click', );
-
-const listsB = document.getElementById("my_lists");
-listsB.addEventListener('click', );
-
-const favoritesB = document.getElementById("my_favorites");
-favoritesB.addEventListener('click', );
-
-const likesB = document.getElementById("my_likes");
-likesB.addEventListener('click', );
-
-const donationsB = document.getElementById("my_donations");
-donationsB.addEventListener('click', );
-
-const signoutB = document.getElementById("signout");
-signoutB.addEventListener('click', );
+const addrowB = document.getElementById("add_row");
+addrowB.addEventListener('click', );
