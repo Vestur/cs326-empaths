7164e6e85beab8090cbdcc99d2b32f29b5d78949
Mathew Han <mathewjhan@gmail.com>
2022-04-19 22:12:46 -0400

    Add faker for charities, WIP in likes.html/likes.js

--- static/likes.js
+++ static/likes.js
@@ -3,10 +3,40 @@ async function getLikedCharities() {
       method: 'GET',
   });
   const data = await response.json();
+  console.log("hi", data);
   return data;
 }
 
-function createCharityCard() {
+function createCharityCard(charity) {
+  const card = document.createElement("div");
+  card.classList.add("card", "charitable-card");
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
+  const cardTitle = document.createElement("div");
+  cardTitle.classList.add("card-title", "charitable-card-title", "d-flex", "justify-content-between");
+  const titleText = document.createElement("span");
+  titleText.innerHTML = charity.name;
+  const likes = document.createElement("button");
+  likes.classList.add(["btn"]);
+  likes.innerHTML = `${charity.likes} 💛`;
+
+  cardTitle.appendChild(titleText);
+  cardTitle.appendChild(likes);
+  const info = document.createElement("div");
+  info.classList.add("card-text", "charitable-card-text");
+  info.innerHTML = charity.mission;
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(info);
+
+  card.appendChild(cardBody);
+  return card;
 }
-const charities = getLikedCharities();
+
+const charities = await getLikedCharities();
+const likesWrapper = document.getElementById("likes-wrapper");
+
+for(let charity of charities) {
+  likesWrapper.appendChild(createCharityCard(charity));
+}
