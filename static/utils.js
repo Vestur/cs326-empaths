26ddf10f3a2bf682d7da6ecd96c1788f5a734643
Mathew Han <mathewjhan@gmail.com>
2022-04-19 22:50:58 -0400

    Update cards

--- static/utils.js
+++ static/utils.js
@@ -2,13 +2,29 @@ export function createCharityCard(charity) {
 
     const card = document.createElement("div");
     card.classList.add("card", "charitable-card");
+
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+    cardHeader.innerHTML = `Rating - ${charity.current_rating}`;
+
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
     const cardTitle = document.createElement("div");
     cardTitle.classList.add("card-title", "charitable-card-title", "d-flex", "justify-content-between");
+    const wrapper = document.createElement("div");
+
     const titleText = document.createElement("span");
-    titleText.innerHTML = charity.name;
+    titleText.innerHTML = `${charity.name}`;
+    const favorite = document.createElement("button");
+    favorite.classList.add("btn");
+
+    // Replace with function that determines which emoji
+    favorite.innerHTML = "⭐";
+
+    wrapper.appendChild(favorite);
+    wrapper.appendChild(titleText);
+
     const likes = document.createElement("button");
     likes.classList.add(["btn"]);
     likes.innerHTML = `${charity.likes} 💛`;
@@ -17,7 +33,7 @@ export function createCharityCard(charity) {
     cardSubtitle.classList.add("card-subtitle", "mb-2", "text-muted");
     cardSubtitle.innerHTML = charity.address;
 
-    cardTitle.appendChild(titleText);
+    cardTitle.appendChild(wrapper);
     cardTitle.appendChild(likes);
     const info = document.createElement("div");
     info.classList.add("card-text", "charitable-card-text");
@@ -27,6 +43,7 @@ export function createCharityCard(charity) {
     cardBody.appendChild(cardSubtitle);
     cardBody.appendChild(info);
 
+    card.appendChild(cardHeader);
     card.appendChild(cardBody);
     return card;
 }
